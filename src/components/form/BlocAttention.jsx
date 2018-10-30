e30a24fd23cbfe3b13c2a257e5f8411665d813b6
Ammar DGHAIS <97376r@imac-df.maif.local>
2018-10-30 14:48:05 +0100

    MAJ bloc-attention (Version 1)

--- src/components/form/BlocAttention.jsx
+++ src/components/form/BlocAttention.jsx
@@ -4,14 +4,10 @@ import BlocAttention from "maif-framework-presentation-react/lib/components/form
 
 function BlocAttentionExample() {
   return (
-    <div className="row">
-      <div className="col-md-8 col-sm-10 col-center">
-        <div className="col-form-container">
-          <form className="maif-form">
-            <BlocAttention title="ATTENTION :">Message possible sur plusieurs lignes</BlocAttention>
-          </form>
-        </div>
-      </div>
+    <div className="col-form-container">
+      <form className="maif-form">
+        <BlocAttention title="ATTENTION :">Message possible sur plusieurs lignes</BlocAttention>
+      </form>
     </div>
   );
 }
