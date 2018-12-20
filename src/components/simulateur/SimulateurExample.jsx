51997aa4405fef528b1d63535dcbc4db1928583a
Julien Monchany <julien.monchany@externe.maif.fr>
2018-12-20 16:53:20 +0100

    Add SliderValeur example doc

--- src/components/simulateur/SimulateurExample.jsx
+++ src/components/simulateur/SimulateurExample.jsx
@@ -3,8 +3,25 @@ import PropTypes from "prop-types";
 
 import SliderValeur from "maif-framework-presentation-react/lib/components/simulateur/sliderValeur";
 
-function SimulateurExample(){
-  return <SliderValeur labels={["5 ans", "7 ans", "10 ans", "20 ans", "30 ans"]} />;
+class SimulateurExample extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      value: 1
+    }
+  }
+
+  render() {
+    const testValues = ["5 ans", {label: "7 ans"}, {label: "10 ans", id: "7357"}];
+    return (
+      <React.Fragment>
+        <SliderValeur value={this.state.value} labels={testValues} onChange={value => this.setState({value})}/>
+        <hr />
+        <em>Value: </em><code>{JSON.stringify(testValues[this.state.value], 2)}</code>
+      </React.Fragment>
+    );
+  }
 }
 
 export default SimulateurExample;
